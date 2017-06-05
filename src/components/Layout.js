ade76c857156a0e12312b11a4c9396ce26213325
xhitm3n <goncaloggsantos@gmail.com>
2017-06-05 23:24:00 +0100

    Reworked Grid with content

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,26 +3,25 @@ import Sidebar from './Sidebar';
 import Appbar from './Appbar';
 import { Grid } from 'semantic-ui-react';
 
+const style = {
+    marginLeft: '150px',
+    marginRight: '50px',
+    paddingLeft: '50px'
+};
 export default class Layout extends Component {
     render () {
         return (
-          <div>
-            <Grid>
-                <Grid.Row columns={1}>
-                        <Grid.Column floated='right'>
-                        <Appbar />
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row columns={2}>
-                    <Grid.Column>
-                        <Sidebar />
-                    </Grid.Column>
+        <div>
+            <Appbar />
+            <Sidebar />
+            <Grid style={style}>
+                <Grid.Row>
                     <Grid.Column>
                         {this.props.children}
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
-          </div>
+        </div>
         );
     }
 }
