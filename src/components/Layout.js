643d0fd473e22ba84086b9d92992e88ed6e7812b
xhitm3n <goncaloggsantos@gmail.com>
2017-06-05 20:04:29 +0100

    Added Grid system

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import Sidebar from './Sidebar';
 import Appbar from './Appbar';
-import { Container } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 export default class Layout extends Component {
     render () {
         return (
           <div>
-            <Appbar />
-            <Sidebar/>
-            <Container>
-            {this.props.children}
-            </Container>
+            <Grid>
+                <Grid.Row columns={1}>
+                        <Grid.Column floated='right'>
+                        <Appbar />
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={2}>
+                    <Grid.Column>
+                        <Sidebar />
+                    </Grid.Column>
+                    <Grid.Column>
+                        {this.props.children}
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
           </div>
         );
     }
