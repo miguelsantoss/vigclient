8778f6279e0a86ed9829f26f41534cdc53bfa011
xhitm3n <goncaloggsantos@gmail.com>
2017-06-05 19:39:29 +0100

    Reworked Appbar

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 
-export default class MenuExampleInverted extends Component {
-    state = { activeItem: 'home' }
-
+export default class AppBar extends Component {
+    state = {}
     handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
     render () {
         const { activeItem } = this.state;
-
         return (
           <Menu inverted>
-            <Menu.Item position='right' as={NavLink} to='/login' activeClassName='active'>Log out</Menu.Item>
+            <Menu.Item position='right' name='user' active={activeItem === 'browse'} onClick={this.handleItemClick} as={NavLink} to='/login'>
+              Log Out
+            </Menu.Item>
           </Menu>
         );
     }
