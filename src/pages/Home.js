3cd456c91f5a17cabb04bf350540cc755c8c7b5e
Miguel Santos <miguel.sl233@gmail.com>
2017-06-07 17:45:31 +0100

    debug link to scan 10

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => (
     <div>
       <h1> HELLO HOME </h1>
+      <Link to='/scan/10'>SCAN 10 LINK - only one with machines</Link>
     </div>
 )
 
