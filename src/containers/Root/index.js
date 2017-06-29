d3969cc9f285ee59c68d4a7bfdb63b223b9603de
Miguel Santos <miguel.sl233@gmail.com>
2017-06-29 16:40:06 +0100

    Adding Redux

---
+++ src/containers/Root/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import Router from '../../routes';
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    {Router}
+  </Provider>
+);
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired
+};
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './routes';
+import Root from './containers/Root';
 import 'semantic-ui-css/semantic.min.css';
 
 const mountNode = document.getElementById('root');
