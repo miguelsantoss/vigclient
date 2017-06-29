59ef2daf58f28c62f03aae9084a45e65c2774fc8
Miguel Santos <miguel.sl233@gmail.com>
2017-06-29 21:26:57 +0100

    server init

--- src/server.js
+++ src/server.js
@@ -2,15 +2,16 @@
 import express from 'express';
 // import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import compress from 'compress';
+import compression from 'compression';
+import mysql from 'mysql';
 // import fetch from 'node-fetch';
 import config from './config';
 
 const app = express();
 
-app.use(compress());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(compression());
 
 app.listen(config.port, () => {
   console.info(`The server is running at http://localhost:${config.port}/`);
