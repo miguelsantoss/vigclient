3388a00296b1322d873bdd41edec9f67f009e870
miguelsantoss <miguel.sl233@gmail.com>
2017-08-23 15:32:05 +0100

    service worker + start script update(join client and server)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import Root from './components/containers/Root';
 import store from './store';
+import registerServiceWorker from './utils/registerServiceWorker';
 
 const mountNode = document.getElementById('root'); // eslint-disable-line no-undef
 // eslint-disable-next-line react/no-render-return-value
 const render = NewApp => ReactDOM.render(<NewApp store={store} />, mountNode);
 render(Root);
+
+registerServiceWorker();
+
