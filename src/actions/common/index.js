5c246c631e08976ac8d48739b15d7bcdd31d383e
miguelsantoss <miguel.sl233@gmail.com>
2017-08-23 17:03:34 +0100

    fixed production build

--- src/actions/common/index.js
+++ src/actions/common/index.js
@@ -2,7 +2,6 @@
 // extension your APP_INIT should be look like:
 // export const APP_INIT = '@@INIT'
 export const APP_INIT = '@@redux/INIT'; // eslint-disable-line import/prefer-default-export
-export const RESET_STATE = 'RESET_STATE';
 
-export const RESET_STATE_STORE = () =>
-  dispatch => dispatch({ type: RESET_STATE });
+export const RESET_STATE = 'RESET_STATE';
+export const RESET_STATE_STORE = () => dispatch => dispatch({ type: RESET_STATE });
