e77ea527af539a1e6eddbead31590cb96e3fadd9
miguelsantoss <miguel.sl233@gmail.com>
2017-08-29 10:12:12 +0100

    basic config file

---
+++ src/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // Node.js app
+  port: process.env.PORT || 8080,
+
+  // API Gateway
+  api: {
+    // API URL to be used in the client-side code
+    clientUrl: process.env.API_CLIENT_URL || '',
+    // API URL to be used in the server-side code
+    serverUrl:
+      process.env.API_SERVER_URL ||
+      `http://localhost:${process.env.PORT || 3000}`,
+  },
+
+  // Authentication
+  auth: {
+    jwt: { secret: process.env.JWT_SECRET || 'React Starter Kit' },
+  },
+};
--- src/server/index.js
+++ src/server/index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import morgan from 'morgan';
-
 import routes from './routes';
+import config from '../config';
 
 const app = express();
 
@@ -14,7 +14,9 @@ app.use(bodyParser.json());
 app.use(compression());
 app.use('/api', routes);
 
-const server = app.listen(8080, () => {
+const server = app.listen(config.port, () => {
   const port = server.address().port;
   console.info(`App now running on port ${port}`);
 });
+
+export default server;
